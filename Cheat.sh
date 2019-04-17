1f5156c53132bdcbeafe80697bd0fef8a8304144
Ian Oberdorf <40533534+IanOberdorf@users.noreply.github.com>
2019-04-17 11:28:24 -0400

    Update Cheat.sh

--- Cheat.sh
+++ Cheat.sh
@@ -9,6 +9,7 @@ cd /etc/dhcp
 echo -e '\ndefault-lease-time 600; \nmax-lease-time 7200; \n\nddns-update-style interim; \nignore client-updates; \n\nsubnet 192.168.100.0 netmask 255.255.255.0 { \n range 192.168.100.12 192.168.100.254; \noption routers 192.168.100.1; \noption domain-name "linux.net"; \noption domain-name-servers 192.168.100.10; \n}' >> ./dhcpd.conf
 echo Restarting DHCPD
 systemctl start dhcpd.service
-sleep 20
+echo Waiting for service to complete system start
+sleep 10
 systemctl status dhcpd.service
 echo -e '\n\nIf the previous shows red text stating "\e[91mon ftp.isc.org. Features have been added and other changes have been made to the base software release in order to make it work better with this distribution." \e[39mThen restart the lab.'
